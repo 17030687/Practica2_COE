e391a084872031ee99f9abbc727881a633ce32ed
17030687 <17030687@itesa.edu.mx>
2023-10-18 17:56:22 -0600

    Versión02 SplasScreen

--- ControlAdmin_COE/ControlAdmin_COE.Android/MainActivity.cs
+++ ControlAdmin_COE/ControlAdmin_COE.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ControlAdmin_COE.Droid
 {
-    [Activity(Label = "ControlAdmin_COE", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ControlAdmin_COE", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
